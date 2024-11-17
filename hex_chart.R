3ccc251ae1ff543baf178ad1a94df5d900ef456f
Nate <natebender.mt@gmail.com>
2024-11-17 10:54:00 -0700

    orig palette

--- hex_chart.R
+++ hex_chart.R
@@ -141,7 +141,7 @@ generate_hex_chart = function(hex_data, base_court, court_theme = court_themes$d
       size = court_theme$hex_border_size,
       color = court_theme$hex_border_color
     ) +
-    scale_fill_viridis_d(
+    scale_fill_viridis_c(
       paste0(fill_label, "   "),
       limit = fill_limit,
       labels = label_formatter,
